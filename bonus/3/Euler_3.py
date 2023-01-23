25606e6207af1c882ba6e5d9331004b3670fc1d4
John Fox <107318784+jff97@users.noreply.github.com>
2023-01-23 14:52:15 -0600

    Update Euler_3.py

--- bonus/3/Euler_3.py
+++ bonus/3/Euler_3.py
@@ -1,19 +1,20 @@
 def isPrime(num):
-   for i in range(2, num // 2 + 1, 1):
+   for i in range(2, (num // 2) + 1, 1):
       if num % i == 0:
          return(False)
    return(True)
 
 def lrgstPrimeFac(num):
-   for i in range((num // 2) + 1, 0, -1):
-      print(i)
-      if num % i == 0 and isPrime(i):
+   for i in (primeList(num)).reverse():
+      if num % i == 0:
          return(i)
 
 #
 def primeList(num):
    list = []
-   for i in range(0, (num // 2) + 1, 1):
-      if 
-      list.append()
+   for i in range(2, (num // 2) + 1, 1):
+      if isPrime(i):
+         list.append(i)
    return(list)
+
+print(lrgstPrimeFac(131235))
