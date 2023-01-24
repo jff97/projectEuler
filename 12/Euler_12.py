d6d9cd376b055caa098377c89b49c3c36d99d604
John Fox <107318784+jff97@users.noreply.github.com>
2023-01-24 11:51:11 -0600

    Update Euler_12.py

--- 12/Euler_12.py
+++ 12/Euler_12.py
@@ -1,17 +1,23 @@
-def getDivisors(number):
-   numDivisors = 0
-   for i in range(1, number + 1, 1):
-      if number % i == 0:
-         numDivisors = numDivisors + 1
-   return numDivisors
+import math
+def getDivisors(n):
+   numDiv = 0
+   for i in range(1,math.floor(1 + math.sqrt(n))):
+      if n % i == 0:
+         if n / i == i:
+            numDiv = numDiv + 1
+         else:
+            numDiv = numDiv + 2
+   return numDiv
 
-def trianglular(index):
-   return((index *(index + 1)) / 2)
+def trianglular(curNumber, index):
+   return(curNumber + index)
 
 def firstToNDivisors(n):
    i = 1
-   while getDivisors(trianglular(i)) <= n:
+   curTriangle = 0
+   while getDivisors(trianglular(curTriangle, i)) <= n:
+      curTriangle = trianglular(curTriangle, i)
       i = i + 1
-   return trianglular(i)
+   return trianglular(curTriangle, i)
 
-print(firstToNDivisors(5))
+print(firstToNDivisors(500))
